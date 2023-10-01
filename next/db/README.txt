3b8b07752547c01722bd3a8101da01fe7e207f97
MolinaFranco <franco13mm@gmail.com>
2023-10-01 20:25:02 -0300

    empezando con rtk querys, moqueando el back

---
+++ next/db/README.txt
@@ -0,0 +1,3 @@
+con esto simulamos el backend para test
+corremos con $npm run back
+y levantara el script del package.json llamado back el cual simulara devovler el json
--- next/src/components/layouts/home/GameList.tsx
+++ next/src/components/layouts/home/GameList.tsx
@@ -1,35 +1,16 @@
 import { Box, Button, Card, CardBody, Flex, Heading, Spacer, Text } from "@chakra-ui/react";
 import { } from "path";
 import { FC, ReactElement, useState } from "react";
+import { useGetGamesQuery } from "store/gamesListSlice";
 
 interface IProps {
     children: ReactElement;
 }
 
-const LOBBYS = [
-    {
-        id: 1,
-        count: 5,
-        name: 'game1',
-        max: 12,
-    },
-    {
-        id: 2,
-        count: 10,
-        name: 'game2',
-        max: 12,
-    },
-    {
-        id: 3,
-        count: 12,
-        name: 'game3',
-        max: 12,
-    },
-]
 
 export default function DefaultLayout() {
 
-    const [lobbys, setlobbys] = useState([]);
+    const { data, isError, isLoading, error } = useGetGamesQuery();
 
     return (
         <Box flexBasis="65%" px={16} py={10}>
@@ -48,7 +29,7 @@ export default function DefaultLayout() {
                 rowGap={8}
                 justify="space-between"
             >
-                {lobbys.map(({ id, name, count, max }) => {
+                {data.map(({ id, name, count, max }) => {
                     return (
                         <Box key={id} flexBasis="100%">
                             <GameCard id={id} name={name} count={count} max={max} />
