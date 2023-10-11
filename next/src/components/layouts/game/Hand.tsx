a9b347ff80a04f4c8e86ecf2e1dd9eb783b0d255
MolinaFranco <franco13mm@gmail.com>
2023-10-11 00:34:19 -0300

    hand y carta terminado con sus estados y redux falta test

--- next/src/components/layouts/game/Hand.tsx
+++ next/src/components/layouts/game/Hand.tsx
@@ -1,5 +1,4 @@
 import { Box, Center, Flex, Grid, HStack, SimpleGrid } from "@chakra-ui/react";
-import { } from "path";
 import { FC } from "react";
 import { useSelector } from "react-redux";
 import { RootState } from "@/src/store/store";
@@ -7,20 +6,22 @@ import GameCard from "./GameCard";
 
 type HandProps = {};
 
-const Hand: FC<HandProps> = ({ }) => {
+const Hand: FC<HandProps> = ({}) => {
   const personalData = useSelector((state: RootState) => state.game.playerData);
   const cardsList = personalData.cards;
 
   return (
-      <HStack h='auto' maxW='80vw' data-testid={`HAND`} justify='center' bgColor='blue'>
+    <Center h='100%' >
+      <HStack h='100%' justify='center'>
         {cardsList?.map(({ id, name }) => {
           return (
-            <Box maxW="20%" alignSelf="stretch" h="auto" key={id}>
-              <GameCard id={id} data-testid={`HAND_CARD_${id}`} name={name} />
+            <Box maxW="20%" alignSelf="stretch"  h="auto" key={id}>
+              <GameCard id={id} data-testid={"Hand_card_" + id} name={name} />
             </Box>
           );
         })}
       </HStack>
+    </Center>
   );
 };
 
