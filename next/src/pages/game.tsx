85a80acfe235b8fcb19a1a52a0fae8ba7fa87a2c
wilitp <guilledeipola@gmail.com>
2023-10-11 15:36:36 -0300

    listener basico y test en pagina game

--- next/src/pages/game.tsx
+++ next/src/pages/game.tsx
@@ -1,7 +1,23 @@
 import { PageWithLayout } from "@/src/pages/_app";
 import { Box } from "@chakra-ui/react";
+import { useToast } from "@chakra-ui/react";
+import { useEffect } from "react";
+import { buildSucessToastOptions } from "@/src/utils/toasts";
+import useGameSocket from "@/src/hooks/useGameSocket";
+import { EventType } from "@/src/business/game/gameAPI/listener";
 
 const Page: PageWithLayout = () => {
+  const toast = useToast();
+  const { gameSocket } = useGameSocket();
+  const roomStartHandler = () => {
+    toast(buildSucessToastOptions({ description: "Partida iniciada" }));
+  }
+  useEffect(() => {
+    gameSocket.on(EventType.ON_ROOM_START_GAME, roomStartHandler);
+    return () => {
+      gameSocket.removeListener(EventType.ON_ROOM_START_GAME, roomStartHandler);
+    }
+  });
   return <Box pos="relative">El juego!</Box>;
 };
 
