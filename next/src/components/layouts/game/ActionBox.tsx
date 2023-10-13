c65542547a13460648b3ed1410aa42af55126b2b
Tomás Ponce Gessi <tomaspgwk@gmail.com>
2023-10-13 19:25:30 -0300

    Fixes de layout para la pantalla principal del juego

--- next/src/components/layouts/game/ActionBox.tsx
+++ next/src/components/layouts/game/ActionBox.tsx
@@ -1,42 +1,48 @@
-import { Box, Text, Center, Flex, Grid, HStack, SimpleGrid, Button, Stack } from "@chakra-ui/react";
-import { } from "path";
+import { Button, Stack } from "@chakra-ui/react";
+import {} from "path";
 import { FC } from "react";
 import { useSelector } from "react-redux";
 import { RootState } from "@/src/store/store";
-import GameCard from "./GameCard";
-import { GiBroadsword, GiFireShield, GiSwitchWeapon, GiChaliceDrops } from "react-icons/gi";
-import GreyBG from '@/public/game/grey-back.jpg'
-import BgImage from "@/components/utility/BgImage";
-import { gameSocket } from "business/game/gameAPI";
+import {
+  GiBroadsword,
+  GiFireShield,
+  GiSwitchWeapon,
+  GiChaliceDrops,
+} from "react-icons/gi";
 import { sendPlayerPlayCard } from "business/game/gameAPI/manager";
 
 type ActionBoxProps = {};
 
-const ActionBox: FC<ActionBoxProps> = ({ }) => {
-    const personalData = useSelector((state: RootState) => state.game.playerData);
-    const cardsList = personalData.cards;
+const ActionBox: FC<ActionBoxProps> = ({}) => {
+  const playerData = useSelector((state: RootState) => state.game.playerData);
 
-    return (
-        <>
-            <Stack bgColor='red' h='100%' maxW='20vw' data-testid={`HAND`} justify='center'>
-                <Button colorScheme='whiteAlpha' data-testid='ACTION_BOX_PLAY_BTN' onClick={playCard} rightIcon={<GiBroadsword />} >Jugar</Button>
-                {/* <Button colorScheme='whiteAlpha' rightIcon={<GiChaliceDrops />}>Descartar</Button> */}
-                {/* <Button colorScheme='whiteAlpha' rightIcon={<GiFireShield />}>Defenderse</Button> */}
-                {/* <Button colorScheme='whiteAlpha' rightIcon={<GiSwitchWeapon />}>Intercambiar</Button> */}
-            </Stack>
-        </>
-    );
+  const playCard = () => {
+    var cardSelected = playerData.cardSelected;
+    var playerSelected = playerData.playerSelected;
+
+    var cardOptions = playerSelected ? { target: playerSelected } : {};
+    if (cardSelected !== undefined) {
+      sendPlayerPlayCard(cardSelected, cardOptions);
+    }
+  };
+
+  return (
+    <>
+      <Stack maxW="20vw" data-testid={`HAND`} justify="center">
+        <Button
+          colorScheme="whiteAlpha"
+          data-testid="ACTION_BOX_PLAY_BTN"
+          onClick={playCard}
+          rightIcon={<GiBroadsword />}
+        >
+          Jugar
+        </Button>
+        {/* <Button colorScheme='whiteAlpha' rightIcon={<GiChaliceDrops />}>Descartar</Button> */}
+        {/* <Button colorScheme='whiteAlpha' rightIcon={<GiFireShield />}>Defenderse</Button> */}
+        {/* <Button colorScheme='whiteAlpha' rightIcon={<GiSwitchWeapon />}>Intercambiar</Button> */}
+      </Stack>
+    </>
+  );
 };
 
 export default ActionBox;
-
-const playCard = () => {
-    const playerData = useSelector((state: RootState) => state.game.playerData);
-    var cardSelected = playerData.cardSelected;
-    var playerSelected = playerData.playerSelected;
-    
-    var cardOptions = playerSelected? {target: playerSelected} : {};
-    if(cardSelected!==undefined) {
-        sendPlayerPlayCard(cardSelected, cardOptions);
-    }    
-};
---
+++ next/src/components/utility/Image.tsx
@@ -0,0 +1,10 @@
+import {default as NextImage} from "next/image";
+import { chakra } from "@chakra-ui/react";
+
+const NextImageUsedProps = ["height", "width", "quality", "src", "alt", "title", "priority", "placeholder"];
+
+const Image = chakra(NextImage, {
+  shouldForwardProp: (prop) => NextImageUsedProps.includes(prop),
+});
+
+export default Image;
