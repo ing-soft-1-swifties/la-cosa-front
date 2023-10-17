0f4ea523b8b673beb6856657ac8c95f384d6c701
MolinaFranco <franco13mm@gmail.com>
2023-10-17 00:11:41 -0300

    no se puede jugar la cosa

--- next/src/components/layouts/game/ActionBox.tsx
+++ next/src/components/layouts/game/ActionBox.tsx
@@ -15,32 +15,34 @@ type ActionBoxProps = {};
 
 const ActionBox: FC<ActionBoxProps> = ({ }) => {
   const player = usePlayerGameState();
-  const cardSelected = player.selections.card?.id;
+  const cardSelected = player.selections.card;
+  const cardSelectedID = cardSelected?.id;
   const turn = player.turn;
 
   const playCard = () => {
     const playerSelected = player.selections.player;
 
     var cardOptions = playerSelected ? { target: playerSelected } : {};
-    if (cardSelected !== undefined) {
-      sendPlayerPlayCard(cardSelected, cardOptions);
+    if (cardSelectedID !== undefined) {
+      sendPlayerPlayCard(cardSelectedID, cardOptions);
     }
   };
 
   const discardCard = () => {
-    if (cardSelected !== undefined) {
-      sendPlayerDiscardCard(cardSelected);
+    if (cardSelectedID !== undefined) {
+      sendPlayerDiscardCard(cardSelectedID);
     }
   };
 
   const swapCard = () => {
-    if (cardSelected !== undefined) {
-      sendPlayerSelectExchangeCard(cardSelected);
+    if (cardSelectedID !== undefined) {
+      sendPlayerSelectExchangeCard(cardSelectedID);
     }
   };
   if (player.status == PlayerStatus.DEATH) {
     return null;
   }
+
 
   return (
     <>
@@ -50,16 +52,15 @@ const ActionBox: FC<ActionBoxProps> = ({ }) => {
           data-testid="ACTION_BOX_PLAY_BTN"
           onClick={playCard}
           rightIcon={<GiBroadsword />}
-          isDisabled={cardSelected == undefined && turn !== PlayerTurnState.PLAY_OR_DISCARD}
+          isDisabled={cardSelectedID == undefined && turn !== PlayerTurnState.PLAY_OR_DISCARD && cardSelected?.name == "La cosa"}
         >Jugar
         </Button>
-
         <Button
           colorScheme='whiteAlpha'
           data-testid="ACTION_BOX_DSC_BTN"
           rightIcon={<GiChaliceDrops />}
           onClick={discardCard}
-          isDisabled={cardSelected == undefined && turn !== PlayerTurnState.PLAY_OR_DISCARD}
+          isDisabled={cardSelectedID == undefined && turn !== PlayerTurnState.PLAY_OR_DISCARD && cardSelected?.name == "La cosa"}
         >Descartar
         </Button>
         {/* <Button colorScheme='whiteAlpha' rightIcon={<GiFireShield />}>Defenderse</Button> */}
@@ -68,7 +69,7 @@ const ActionBox: FC<ActionBoxProps> = ({ }) => {
           data-testid="ACTION_BOX_SWAP_BTN"
           rightIcon={<GiSwitchWeapon />}
           onClick={swapCard}
-          isDisabled={cardSelected == undefined && turn !== PlayerTurnState.SELECT_EXCHANGE_CARD}
+          isDisabled={cardSelectedID == undefined && turn !== PlayerTurnState.SELECT_EXCHANGE_CARD && cardSelected?.name == "La cosa"}
         >Intercambiar
         {turn}</Button>
       </Stack>
