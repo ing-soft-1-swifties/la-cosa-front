1b9b1bd56bfdadc893be97c2f5d6e8a7e0ea4966
wilitp <guilledeipola@gmail.com>
2023-10-23 19:25:50 -0300

    Popover con info sobre acciones disponibles

--- next/src/components/layouts/game/ActionBox.tsx
+++ next/src/components/layouts/game/ActionBox.tsx
@@ -1,5 +1,4 @@
-import { Button, Stack } from "@chakra-ui/react";
-import { } from "path";
+import { Button, Text, Popover, PopoverBody, Stack, PopoverTrigger, PopoverContent, PopoverHeader, Box, } from "@chakra-ui/react";
 import { FC } from "react";
 import {
   GiBroadsword,
@@ -18,7 +17,7 @@ const ActionBox: FC<ActionBoxProps> = ({ }) => {
   const player = usePlayerGameState();
   const cardSelected = player.selections.card;
   const cardSelectedID = cardSelected?.id;
-  const turn = player.turn;
+  const {turn, on_exchange, on_turn} = player;
 
   const playCard = () => {
     const playerSelected = player.selections.player;
@@ -51,59 +50,95 @@ const ActionBox: FC<ActionBoxProps> = ({ }) => {
     return null;
   }
 
+  let popoverTitle = "No estás en tu turno!";
+  let popoverText = "Debes esperar la acción de otros jugadores.";
+
+  if (on_turn && !on_exchange) {
+    popoverTitle = "Tu turno!";
+    popoverText = "Elije una carta para jugar o descartar.";
+  } else if(on_turn && on_exchange) {
+    popoverTitle = "Ofrece un intercambio!";
+    popoverText = "Elije una carta para intercambiar con el otro jugador.";
+  } else if(!on_turn && on_exchange) {
+    popoverTitle = "Te han ofrecido un intercambio!";
+    popoverText = "Elije una carta para intercambiar o para defenderte del el otro jugador.";
+  }
+
   return (
-    <>
-      <Stack maxW="20vw" data-testid={`HAND`} justify="center">
-        <Button
-          colorScheme="whiteAlpha"
-          data-testid="ACTION_BOX_PLAY_BTN"
-          onClick={playCard}
-          rightIcon={<GiBroadsword />}
-          isDisabled={cardSelectedID == undefined ||
-            cardSelected?.name == Card.THETHING ||
-            cardSelected?.name == Card.INFECTED ||
-            (cardSelected?.needTarget && player.selections.player == undefined)
-          }
-        // TODO: a futuro nos fijamos en PlayerTurnState
-        // isDisabled={cardSelectedID == undefined && turn !== PlayerTurnState.PLAY_OR_DISCARD && cardSelected?.name == Card.THETHING}
-        >Jugar
-        </Button>
+    <Box mx="5">
+      <Popover isOpen placement="top">
+        <PopoverContent 
+          bg="rgba(0, 0, 0, 0.4)" 
+          color="white" 
+          borderWidth="1px"
+          borderColor="gray"
+        >
+          <PopoverHeader borderColor="gray">{popoverTitle}</PopoverHeader>
+          <PopoverBody>{popoverText}</PopoverBody>
+        </PopoverContent>
+        <PopoverTrigger>
+          <Stack maxW="20vw" data-testid={`HAND`} justify="center">
+            {
+              on_turn && !on_exchange && <>
+                <Button
+                  colorScheme="whiteAlpha"
+                  data-testid="ACTION_BOX_PLAY_BTN"
+                  onClick={playCard}
+                  rightIcon={<GiBroadsword />}
+                  isDisabled={cardSelectedID == undefined ||
+                    cardSelected?.name == Card.THETHING ||
+                    cardSelected?.name == Card.INFECTED ||
+                    (cardSelected?.needTarget && player.selections.player == undefined)
+                  }
+                // TODO: a futuro nos fijamos en PlayerTurnState
+                // isDisabled={cardSelectedID == undefined && turn !== PlayerTurnState.PLAY_OR_DISCARD && cardSelected?.name == Card.THETHING}
+                >Jugar
+                </Button>
 
-        <Button
-          colorScheme='whiteAlpha'
-          data-testid="ACTION_BOX_DSC_BTN"
-          rightIcon={<GiChaliceDrops />}
-          onClick={discardCard}
-          isDisabled={cardSelectedID == undefined || cardSelected?.name == Card.THETHING}
-        >Descartar
-        </Button>
+                <Button
+                  colorScheme='whiteAlpha'
+                  data-testid="ACTION_BOX_DSC_BTN"
+                  rightIcon={<GiChaliceDrops />}
+                  onClick={discardCard}
+                  isDisabled={cardSelectedID == undefined || cardSelected?.name == Card.THETHING}
+                >Descartar
+                </Button>
+              </>
 
-        <Button
-          colorScheme='whiteAlpha'
-          data-testid="ACTION_BOX_DEFENSE_BTN"
-          onClick={defenseCard}
-          rightIcon={<GiFireShield />}
-          isDisabled={cardSelectedID == undefined || cardSelected?.subType !==  CardSubTypes.DEFENSE}
-        >Defenderse
-        </Button>
+            }
 
-        <Button
-          colorScheme='whiteAlpha'
-          data-testid="ACTION_BOX_SWAP_BTN"
-          rightIcon={<GiSwitchWeapon />}
-          onClick={swapCard}
-          isDisabled={
-            cardSelectedID == undefined || 
-            cardSelected?.name == Card.THETHING || 
-            (cardSelected?.name == Card.INFECTED && player.role == PlayerRole.HUMAN)
-            // TODO: la condicion de abajo esta incompleta. 
-            // tambien le falta que sea la unica carta de infectado en la mano
-            // || (cardSelected?.name == Card.INFECTED && player.role == PlayerRole.INFECTED) 
-          }
-        >Intercambiar
-          {turn}</Button>
-      </Stack>
-    </>
+            {
+              on_exchange && !on_turn && <Button
+                colorScheme='whiteAlpha'
+                data-testid="ACTION_BOX_DEFENSE_BTN"
+                onClick={defenseCard}
+                rightIcon={<GiFireShield />}
+                isDisabled={cardSelectedID == undefined || cardSelected?.subType !== CardSubTypes.DEFENSE}
+              >Defenderse
+              </Button>
+            }
+
+            {
+              on_exchange && <Button
+              colorScheme='whiteAlpha'
+              data-testid="ACTION_BOX_SWAP_BTN"
+              rightIcon={<GiSwitchWeapon />}
+              onClick={swapCard}
+              isDisabled={
+                cardSelectedID == undefined ||
+                cardSelected?.name == Card.THETHING ||
+                (cardSelected?.name == Card.INFECTED && player.role == PlayerRole.HUMAN)
+                // TODO: la condicion de abajo esta incompleta. 
+                // tambien le falta que sea la unica carta de infectado en la mano
+                // || (cardSelected?.name == Card.INFECTED && player.role == PlayerRole.INFECTED) 
+              }
+            >Intercambiar
+              {turn}</Button>
+            }
+          </Stack>
+        </PopoverTrigger>
+      </Popover>
+    </Box>
   );
 };
 
