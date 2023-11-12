0422e4f6cd1e2a7aa4c53e38ea18a9d67edfa4c5
Tomas Ponce Gessi <tomaspgwk@gmail.com>
2023-11-12 19:50:24 -0300

    Mostrar avatares con imagenes en el Lobby tmb

--- next/src/components/PlayerCard.tsx
+++ next/src/components/PlayerCard.tsx
@@ -1,19 +1,21 @@
 import { Card, CardBody, Flex, Avatar, Text } from "@chakra-ui/react";
 import { FC } from "react";
+import { GamePlayer } from "@/store/gameSlice";
+import PlayerAvatar from "./utility/PlayerAvatar";
 
 // Props del compononente de una tarjeta de jugador
 type PlayerCardProps = {
-  name: string;
+  player: GamePlayer;
 };
 
 // Componente de una tarjeta de jugador
-const PlayerCard: FC<PlayerCardProps> = ({ name }) => {
+const PlayerCard: FC<PlayerCardProps> = ({ player }) => {
   return (
     <Card pl={6}>
       <CardBody>
         <Flex align="center" columnGap={6}>
-          <Avatar name={name} />
-          <Text>{name}</Text>
+          <PlayerAvatar player={player} isSelected={false} />
+          <Text>{player.name}</Text>
         </Flex>
       </CardBody>
     </Card>
